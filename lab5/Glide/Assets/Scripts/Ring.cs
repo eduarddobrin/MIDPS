9506baa081f6f9324603f9a6d009ad904b0c064e
NegruIgor <potato518403@gmail.com>
2017-05-18 23:47:33 +0300

    Finisam laboratorul 5

---
+++ lab5/Glide/Assets/Scripts/Ring.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ring : MonoBehaviour
+{
+    private Objective objectiveScript;
+    private bool ringActive = false;
+
+    public void Start()
+    {
+        objectiveScript = FindObjectOfType<Objective>();
+    }
+
+    public void ActivateRing()
+    {
+        ringActive = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(ringActive)
+        {
+            objectiveScript.NextRing();
+            Destroy(gameObject, 5.0f);
+        }
+    }
+    
+
+}
